e5aa67d140f45eb09921da38b158216749e96537
hh1199 <hieuchu99@gmail.com>
2022-07-13 00:36:11 +0700

    update

---
+++ src/AuthComponent.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { Button } from "react-bootstrap";
+const cookies = new Cookies();
+
+const token = cookies.get("TOKEN");
+
+export default function AuthComponent() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const configuration = {
+      method: "get",
+      url: "https://node-auth-be.herokuapp.com/auth-endpoint",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    axios(configuration)
+      .then((result) => {
+        setMessage(result.data.message);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  }, []);
+  const logout = () => {
+    cookies.remove("TOKEN", { path: "/" });
+    window.location.href = "/";
+  };
+
+  return (
+    <div className="text-center">
+      <h1>Auth Component</h1>
+      <h3 className="text-danger">{message}</h3>
+      <Button type="submit" variant="danger" onClick={() => logout()}>
+        Logout
+      </Button>
+    </div>
+  );
+}
